82d78fe2d3206125d835043f08c86fdc4ff681d6
Lucatel <michal.hal1379@gmail.com>
2025-10-09 11:08:40 +0200

    glowing effect improved

--- source/scripts/StageBox.cs
+++ source/scripts/StageBox.cs
@@ -4,8 +4,9 @@ using System.Threading.Tasks;
 
 public partial class StageBox : Node3D{
 	private Color original_color;
+	private Color glow_color = new Color(0.451f, 0f, 0.617f);
 	private MeshInstance3D mesh;
-	private int delay_ms = 100;
+	private int delay_ms = 200;
 	
 	public override void _Ready(){
 		mesh = GetNode<MeshInstance3D>("MeshInstance3D");
@@ -16,16 +17,31 @@ public partial class StageBox : Node3D{
 	
 	}
 	
-	private void change_color(Color c){
+	private StandardMaterial3D change_color(Color c){
 		StandardMaterial3D mat = mesh.GetSurfaceOverrideMaterial(0) as StandardMaterial3D;
+		mat.EmissionEnabled = true;
+		mat.Emission = new Color(0f, 0f, 0f);
 		mat.AlbedoColor = c;
+		mesh.SetSurfaceOverrideMaterial(0, mat);
+		return mat;
+	}
+	
+	public void reset_color(){
+		StandardMaterial3D mat = mesh.GetSurfaceOverrideMaterial(0) as StandardMaterial3D;
+		mat.EmissionEnabled = false;
+		mat.AlbedoColor = original_color;
 		mesh.SetSurfaceOverrideMaterial(0, mat);
 	}
 	
 	public async void start_glow(){
-		change_color(new Color(0, 0, 1));
-		await Task.Delay(delay_ms);
-		change_color(original_color);
+		StandardMaterial3D mat = change_color(glow_color);
+		const int STEPS=10;
+		
+		for(int i=STEPS-1;i>=0;i--){
+			await Task.Delay(delay_ms/STEPS);
+			mat.Emission = new Color(glow_color.R * ((float)i/STEPS),glow_color.G * (float)i/STEPS,glow_color.B * (float)i/STEPS);
+		}
+		reset_color();
 	}
 	
 	public void OnPlayerCollided(CharacterBody3D player){
